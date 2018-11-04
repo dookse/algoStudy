78b23b0cdf6fed221507c00ed91030ae4c1d28b3
jhpark <mugenjh@gmail.com>
2018-11-04 12:54:26 +0900

    lesson4 - PermCheck

---
+++ src/main/codility/lesson4/PermCheck.java
@@ -0,0 +1,21 @@
+package codility.lesson4;
+
+import static java.util.Arrays.sort;
+
+public class PermCheck {
+
+    public int solution(final int[] a) {
+        sort(a);
+        int seq = 1;
+        for (int num : a) {
+            if (isNotSequential(seq++, num)) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    private boolean isNotSequential(final int seq, final int a) {
+        return seq != a;
+    }
+}
